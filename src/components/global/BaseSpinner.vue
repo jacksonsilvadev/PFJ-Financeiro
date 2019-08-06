0c57856bdf00ac6dda216065ea876d440ab25c26
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-08-06 20:56:39 -0300

    Spinner e configurações de variáveis scss

---
+++ src/components/global/BaseSpinner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="base-spinner fa-3x">
+    <i class="fas fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.base-spinner {
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 1000000;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $dark-medium;
+  color: $featured;
+}
+
+</style>
