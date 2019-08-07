7aa41ce6d8a0d0def76ef81da80118d9b8c3a95c
Jackson <jackson.rodrigues9@hotmail.com>
2019-08-07 17:05:40 -0300

    Fazendo a configuração das rotas e do menu sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
   <div id="app">
     <base-spinner />
-    <router-view />
+
+    <div class="container-fluid" v-if="logged">
+      <div class="row">
+        <div class="col-2 navigation-sidebar">
+          <h1 class="app-title">PFJ Financeiro</h1>
+          <layout-navigation />
+        </div>
+        <div class="col">
+          <router-view />
+        </div>
+      </div>
+    </div>
+
+    <router-view v-else />
   </div>
 </template>
 
 <script>
 import BaseSpinner from "./components/global/BaseSpinner";
+import LayoutNavigation from "./components/layout/LayoutNavigation";
 import { setTimeout } from "timers";
 
 export default {
   name: "App",
   components: {
-    BaseSpinner
+    BaseSpinner,
+    LayoutNavigation
+  },
+  data() {
+    return {
+      logged: false
+    };
   },
   mounted() {
     this.$firebase.auth().onAuthStateChanged(user => {
       window.uid = user ? user.uid : null;
+      this.logged = !!user;
 
       this.$router.push({ name: window.uid ? "home" : "login" }, () => {});
 
@@ -40,5 +61,13 @@ export default {
   height: 100vh;
   color: var(--light);
   background-color: var(--darker);
+  .navigation-sidebar {
+    background-color: var(--dark-medium);
+    .app-title {
+      text-align: center;
+      font-size: 20pt;
+      margin-top: 10px;
+    }
+  }
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -4,23 +4,38 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [{
       path: '/',
       name: 'home',
+      meta: {
+        icon: 'home',
+        title: 'Home'
+      },
       component: () => import( /* webpackChunkName: "home" */ './views/home/Home')
+    },
+    {
+      path: '/lista-gastos',
+      name: 'lista-gastos',
+      meta: {
+        icon: 'list-ul',
+        title: 'Listar Gastos'
+      },
+      component: () => import( /* webpackChunkName: "lista-gastos" */ './views/lista-gastos/ListaGastos')
     },
     {
       path: '/login',
       name: 'login',
+      meta: {
+        icon: 'home',
+        title: 'Home'
+      },
       component: () => import( /* webpackChunkName: "login" */ './views/login/Login')
     }
   ]
 })
 
 router.beforeEach((to, from, next) => {
-
+  document.title = `${to.meta.title} - PFJ Financeiro`
   if (!window.uid && to.name !== 'login') {
     next({
       name: 'login'
